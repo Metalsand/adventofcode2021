1c1238b9a9721447938f66b1e9486efcad95bb1c
Metalsand <metalsand450@gmail.com>
2021-12-03 14:23:26 -0500

    test

--- day3/day3.ps1
+++ day3/day3.ps1
@@ -1,5 +1,4 @@
 [char[][]]$inputData = Get-Content .\day3\input.txt
-$inputData[0][1]
 $binaryCount = 0,0,0,0,0,0,0,0,0,0,0,0
 forEach($numArray in $inputData){
     for($i = 0; $i -lt 12; $i++){
@@ -20,4 +19,35 @@ $eBin = forEach($num in $binaryCount){
 }
 $gamma = [convert]::ToInt32([string]-Join $gBin, 2)
 $epsilon = [convert]::ToInt32([string]-Join $eBin, 2)
-Write-Output($gamma*$epsilon)
+Write-Output("Day 3 Part 1: " + $gamma*$epsilon)
+
+#part 2
+#Oxygen generator - Go down the list in iteration; byte[0] for all, then whatever bit is most common in all entries, dump those with that bit into a list, then continue with byte[1]
+
+for($i = 0; $i -lt 1; $i++)
+{
+    if($inputData.Count -ne 1)
+    {
+        $seriesCount = 0;
+        $bitPosition = 0;
+        forEach($numArray in $inputData)
+        {
+            #Write-Output([int32]$numArray[$i]-48) 
+            if([int32]$numArray[$i]-48){$seriesCount++}
+            else{$seriesCount--}
+        }
+        if($seriesCount -gt 0){$bitPosition = 1}
+
+        forEach($numArray in $inputData){
+            Write-Output([String]$([int32]$numArray[$i]-48) + " and bit position is $bitposition")
+            Write-Output($numArray[$i] + $numArray[$i].GetType())
+            if([int32]$numArray[$i]-48 -eq $bitPosition){$numArray}
+        }
+        $inputData = forEach($numArray in $inputData){
+            if([int32]$numArray[$i]-48 -eq $bitPosition){$numArray}
+        }
+        #Write-Output("Total entries in inputData " + $inputData.Count)
+        #Write-Output($bitPosition)
+    }
+}
+Write-Output("Final input data " + $inputData)
