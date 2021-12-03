010e59233e8242acc9509182359072a6d96ea648
Metalsand <metalsand450@gmail.com>
2021-12-03 13:14:04 -0500

    day3part1

--- day2/day2.ps1
+++ day2/day2.ps1
@@ -1,15 +1,49 @@
-$subLocate = forEach($row in $(Import-CSV -Path .\input.txt -Delimiter ' ' -Header $('dir','dist')))
-{
-    [PSCustomObject]@{
-        dir = [String]$row.dir
-        dist = [Int32]$row.dist
+function Part1{
+    $location = @{distance=0;depth=0}
+    $subMoves = forEach($row in $(Import-CSV -Path .\day2\input.txt -Delimiter ' ' -Header $('dir','dist')))
+    {
+        [PSCustomObject]@{
+            dir = [String]$row.dir
+            dist = [Int32]$row.dist
+        }
+    }
+    forEach($move in $subMoves){
+        switch($move.dir)
+        {
+            "up"{$location.depth -= $move.dist}
+            "down"{$location.depth += $move.dist}
+            "forward"{$location.distance += $move.dist}
+            "backward"{$location.distance -= $move.dist}
+        }
+    }
+    Write-Output("Part 1 is " + $location.distance * $location.depth)
+}
+function Part2{
+    $location = @{distance=0;depth=0;tilt=0}
+    $subMoves = forEach($row in $(Import-CSV -Path .\day2\input.txt -Delimiter ' ' -Header $('dir','dist')))
+    {
+        [PSCustomObject]@{
+            dir = [String]$row.dir
+            dist = [Int32]$row.dist
+        }
+    }
+    forEach($move in $subMoves){
+        switch($move.dir)
+        {
+            "up"{$location.tilt -= $move.dist}
+            "down"{$location.tilt += $move.dist}
+            "forward"{
+                $location.distance += $move.dist
+                $location.depth += $($move.dist * $location.tilt)
+                Write-Output($move.dist + " " + $location.tilt)
+            }
+            "backward"{
+                $location.distance -= $move.dist
+                $location.depth -= $move.dist * $move.tilt
+            }
+        }
     }
 }
-$subLocate[0].dist.GetType()
-# $subLocate = $(Import-CSV -Path .\input.txt -Delimiter ' ' -Header $('dir','dist'))
-# $subLocate.dir.GetType()
-# $subLocate.dir = $subLocate.dir -As [string[]]
-# $test = $subLocate.dir -As [string[]], $subLocate.dist -As [int[]]
-# $currentCoordinates = 0,0
-# Write-Output $test
 
+Part1
+Part2
---
+++ day3/day3.ps1
@@ -0,0 +1,23 @@
+[char[][]]$inputData = Get-Content .\day3\input.txt
+$inputData[0][1]
+$binaryCount = 0,0,0,0,0,0,0,0,0,0,0,0
+forEach($numArray in $inputData){
+    for($i = 0; $i -lt 12; $i++){
+        if([int32]$numArray[$i]-48){$binaryCount[$i]++}
+        else{$binaryCount[$i]--}
+    }
+}
+Write-Output($binaryCount)
+$gBin = forEach($num in $binaryCount){
+    if($num -gt 0){
+        1
+    }else{0}
+}
+$eBin = forEach($num in $binaryCount){
+    if($num -lt 0){
+        1
+    }else{0}
+}
+$gamma = [convert]::ToInt32([string]-Join $gBin, 2)
+$epsilon = [convert]::ToInt32([string]-Join $eBin, 2)
+Write-Output($gamma*$epsilon)
